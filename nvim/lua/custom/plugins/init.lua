74c905a6aba32acd678fa4f49ce4c8ef82707cee
Matthew Smith <guitarbass95@gmail.com>
2024-03-02 15:36:18 -0500

    add my nvim stuff

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,25 @@
+
+
+return {
+	{
+		"aserowy/tmux.nvim",
+		config = function() return require("tmux").setup() end
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		requires = {
+			"nvim-tree/nvim-web-devicons",
+		},
+--		config = function() return require("nvim-tree").setup() end
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = 'nvim-tree/nvim-web-devicons',
+--		config = function() return require("bufferline").setup() end
+	},
+
+}
